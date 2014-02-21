a163fc17a3b99f8fec60a98339444caaaf192cb0
Hoa Vo <hvo@solutionsiq.com>
2014-02-21 15:58:17 -0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140221223046) do
+ActiveRecord::Schema.define(:version => 20140221234702) do
 
   create_table "books", :force => true do |t|
+    t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
